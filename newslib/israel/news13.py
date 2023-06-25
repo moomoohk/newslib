5adb5b2bc60f2f014eb198c06038104c9ef9d180
moomoohk <2220203+moomoohk@users.noreply.github.com>
2023-06-26 00:01:34 +0300

    Update news13 source

--- newslib/israel/news13.py
+++ newslib/israel/news13.py
@@ -3,6 +3,7 @@ import re
 from datetime import datetime
 from functools import lru_cache
 
+from bs4 import BeautifulSoup
 from bs4.element import Tag
 
 from newslib.source import Source
@@ -15,7 +16,7 @@ class News13Source(Source):
             root="https://13news.co.il/",
         )
 
-        self.data_query_re = re.compile(r"window\.data_query = (?P<data>.+?)\.data_query")
+        self.root = self._update_root()
 
     @property
     def top_article_selector(self) -> str:
@@ -33,61 +34,68 @@ class News13Source(Source):
     def published_selector(self) -> str:
         raise NotImplementedError
 
+    def _update_root(self):
+        html = self.get_root_content()
+        data = self.get_data(html)
+
+        return data.get("props").get("pageProps").get("baseUrl")
+
     def get_times(self, url, html=None):
         post_data = self.get_post_data(url, html)
 
-        published_timestamp = post_data.get("publishDate_t")
-        updated_timestamp = post_data.get("updateDate_t")
-        if None in (published_timestamp, updated_timestamp):
+        published_text = post_data.get("publishDate")
+        updated_text = post_data.get("updateDate")
+        if None in (published_text, updated_text):
             raise Exception("Malformed JSON (missing times)")
 
-        published = datetime.fromtimestamp(published_timestamp / 1000)
-        updated = None if updated_timestamp == "" else datetime.fromtimestamp(updated_timestamp / 1000)
+        published = datetime.strptime(published_text, "%Y-%m-%d %H:%M:%S")
+        updated = datetime.strptime(updated_text, "%Y-%m-%d %H:%M:%S")
 
         return published, updated
 
     @lru_cache(maxsize=10)
     def get_data(self, html: str):
-        match = re.search(self.data_query_re, html)
-        if not match:
-            raise Exception("Couldn't extract article data")
+        html = BeautifulSoup(html, "lxml")
+        script = html.find("script", attrs={"id": "__NEXT_DATA__"})
+        script = script.contents[0]
 
-        return json.loads(match["data"])
+        return json.loads(str(script))
 
     def get_post_data(self, url, html=None):
         html = self.get_html(url, html)
         data = self.get_data(str(html))
 
-        post_id = data["header"].get("post_id")
-        if post_id is None:
-            raise Exception("Malformed JSON (missing post ID)")
-
-        post_data = data["items"].get(str(post_id))
-        if post_data is None:
-            raise Exception("Malformed JSON (missing post data)")
-
-        return post_data
+        return data.get("props").get("pageProps").get("page").get("Content").get("Item")
 
     @lru_cache(maxsize=10)
     def get_articles(self, html: str):
-        data = self.get_data(html)
+        data = self.get_data(html).get("props").get("pageProps").get("page").get("Content").get("PageGrid")
 
-        blocks = data.get("blocks")
-        if blocks is None:
-            raise Exception("Malformed JSON (no blocks)")
+        standard_four = next(filter(lambda grid_item: grid_item["grid_type"] == "standard_four", data))
 
-        if len(blocks) < 1:
-            raise Exception("Blocks list is too short")
+        posts = standard_four.get("posts")
+        if posts is None:
+            raise Exception("Malformed JSON (no posts)")
 
-        main_block_posts = blocks[0].get("Posts")
-        if main_block_posts is None:
-            raise Exception("Malformed JSON (no main block posts)")
+        if len(posts) < 1:
+            raise Exception("Posts list is too short")
 
-        items = data.get("items")
-        if items is None:
-            raise Exception("Malformed JSON (no items)")
+        return posts
 
-        return [items[str(post_id)] for post_id in main_block_posts]
+    @lru_cache(maxsize=10)
+    def get_top_article(self, html: str):
+        data = self.get_data(html).get("props").get("pageProps").get("page").get("Content").get("PageGrid")
+
+        main_standard = next(filter(lambda grid_item: grid_item["grid_type"] == "MainStandard", data))
+
+        main_posts = main_standard.get("posts")
+        if main_posts is None:
+            raise Exception("Malformed JSON (no MainStandard posts)")
+
+        if len(main_posts) != 1:
+            raise Exception(f"Expected 1 MainStandard post, got {len(main_posts)}")
+
+        return main_posts[0]
 
     @staticmethod
     def create_a(article_data: dict):
@@ -111,11 +119,9 @@ class News13Source(Source):
         elif isinstance(root_content, bytes):
             root_content = root_content.decode()
 
-        articles = self.get_articles(root_content)
-        if len(articles) < 1:
-            raise Exception("Articles list too short")
+        top_article = self.get_top_article(root_content)
 
-        return self.create_a(articles[0])
+        return self.create_a(top_article)
 
     def get_substories_a(self, root_content=None) -> list[Tag]:
         if root_content is None:
@@ -142,7 +148,7 @@ class News13Source(Source):
     def get_category(self, url, html=None) -> str:
         post_data = self.get_post_data(url, html)
 
-        category = post_data.get("CategoryLabel")
+        category = post_data.get("category").get("name")
         if category is None:
             raise Exception("Malformed JSON (missing category)")
 
